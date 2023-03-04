eee667f996c5487a97301cdb705fd6b6d97c128a
Neperoni <90918860+Neperoni@users.noreply.github.com>
2023-03-04 16:21:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const Joi = require('joi');
 
 const createUserSchema = Joi.object({
     userName: Joi.string().required(),
-    email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
 });
 
