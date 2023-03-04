0e35cda03dfbd54512626627a574679f47b21408
Neyha Billing <78521142+ney927@users.noreply.github.com>
2023-03-04 16:35:30 -0500

    mssg js with filler messages

---
+++ pages/client/mssgs.js
@@ -0,0 +1,55 @@
+messages = [
+    {
+        from: "testdata",
+        app: "testdata",
+        title: "testdata",
+        content: "testdata",
+        date: "testdata"
+    },
+    {
+        from: "testdata",
+        app: "testdata",
+        title: "testdata",
+        content: "testdata",
+        date: "testdata"
+    },
+    {
+        from: "testdata",
+        app: "testdata",
+        title: "testdata",
+        content: "testdata",
+        date: "testdata"
+    },
+    {
+        from: "testdata",
+        app: "testdata",
+        title: "testdata",
+        content: "testdata",
+        date: "testdata"
+    }
+];
+
+const all_mssg_div = document.getElementById('all-mssgs');
+
+messages.forEach(element => {
+    const mssg_div = document.createElement('div');
+    mssg_div.className = 'mssg';
+    
+    const header = document.createElement('p');
+    header.className = 'header';
+    header.innerHTML = element.title;
+
+    const content = document.createElement('p');
+    content.className = 'content';
+    content.innerHTML = element.description;
+
+    const info = document.createElement('p');
+    info.className = 'info';
+    info.innerHTML = element.date + '&emsp;'+ element.from + '&emsp;' +element.app;
+
+    mssg_div.appendChild(header);
+    mssg_div.appendChild(content);
+    mssg_div.appendChild(info);
+
+    all_mssg_div.appendChild(mssg_div);
+});
