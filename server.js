ec0e6b9ade107013a5ad332bff858dbf058dc4a3
Neperoni <90918860+Neperoni@users.noreply.github.com>
2023-03-04 16:08:53 -0500

    bug fix wrong name

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Joi = require('joi');
 
 
 const createUserSchema = Joi.object({
-    userName: Joi.string().required(),
+    user: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
 });
