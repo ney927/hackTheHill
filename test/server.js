9bf894ea40ededfbed9d1798f564cf8061a0fce7
Neperoni <90918860+Neperoni@users.noreply.github.com>
2023-03-04 11:44:00 -0500

    what m i even doing anymore

--- test/server.js
+++ test/server.js
@@ -24,10 +24,10 @@ const ROOT_DIR = 'html' //dir to serve static files from
 console.log('Finding database\n')
 
 fs.exists(dataFile, (exists) => {
-  if(exists){
+  if (exists) {
     console.log(dataFile + '<--EXISTS')
     //Found the song file
-    fs.readFile(dataFile, function(err, data) {
+    fs.readFile(dataFile, function (err, data) {
       //Read song data file and send lines and chords to client
       if (err) {
 
@@ -36,9 +36,9 @@ fs.exists(dataFile, (exists) => {
         exit;
 
       } else {
-        
+
         console.log('Loading Data\n')
-        var dataBase =  JSON.parse(data)
+        var dataBase = JSON.parse(data)
         console.log('Successfully Loaded Data\n')
 
       }
@@ -78,7 +78,7 @@ function validateJSON(json, format) {
 
 
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
   let urlObj = url.parse(request.url, true, false)
   console.log('\n============================')
   console.log("PATHNAME: " + urlObj.pathname)
@@ -88,7 +88,7 @@ http.createServer(function(request, response) {
   let receivedData = ''
 
   //attached event handlers to collect the message data
-  request.on('data', function(chunk) {
+  request.on('data', function (chunk) {
     receivedData += chunk
   })
 
@@ -96,19 +96,19 @@ http.createServer(function(request, response) {
   let returnObj = {} //object to be returned to client
 
   //event handler for the end of the message
-  request.on('end', function() {
+  request.on('end', function () {
     console.log('received data: ', receivedData)
     console.log('type: ', typeof receivedData)
 
-    
+
     dataObj = json.parse(urlObj.data)
     returnObj.text = "ERROR"//error until proven otherwise
 
     if (request.method === "POST" && urlObj.pathname === "/addUser") {
-      if(validateJSON(urlObj.data,userFormat)){
+      if (validateJSON(urlObj.data, userFormat)) {
         let addUser = urlObj.data;
         //
-        if(dataBase[addUser.user]===undefined){
+        if (dataBase[addUser.user] === undefined) {
           //username not taken
 
           // Read the JSON file and parse it into an object
@@ -117,104 +117,97 @@ http.createServer(function(request, response) {
 
           //add user to database
           dataBase[addUser.user] = {
-            "password":addUser.password,
-            "events":[],
-            "messages":[]
+            "password": addUser.password,
+            "events": [],
+            "messages": []
           }
 
           // Write the updated object back to the file
           fs.writeFileSync('data.json', JSON.stringify(data));
-                    
+
         }
-        
+
       }
     }
     else if (request.method === "POST" && urlObj.pathname === "/addEvent") {
-      if(validateJSON(urlObj.data,userFormat)){
-        let addUser = urlObj.data;
+      if (validateJSON(urlObj.data, eventFormat)) {
+        let addEvent = urlObj.data;
         //
-        if(dataBase[addUser.user]===undefined){
-          //username not taken
+        if (dataBase[addEvent.user] === undefined) {
+          //username not found
+
+        }
+        else {
 
           // Read the JSON file and parse it into an object
           const rawData = fs.readFileSync('data.json');
           const data = JSON.parse(rawData);
 
-          //add user to database
-          dataBase[addUser.user] = {
-            "password":addUser.password,
-            "events":[],
-            "messages":[]
-          }
+          //add event to user entry in database
+          dataBase[addEvent.user].events.append(addEvent.event)
 
           // Write the updated object back to the file
           fs.writeFileSync('data.json', JSON.stringify(data));
-                    
+
         }
-        
+
       }
     }
 
-    
+    else if (request.method === "POST" && urlObj.pathname === "/getData") {
 
-    
-      //a POST request to fetch a song
-    //look for song file in songs directory based on song title
-    let songFile = `songs/${dataObj.text.trim()}.txt`
-    console.log(`Looking for song file: ${songFile}`)
-    fs.exists(songFile, (exists) => {
-      if(exists){
-        console.log(songFile + '<--EXISTS')
-        //Found the song file
-        fs.readFile(songFile, function(err, data) {
-          //Read song data file and send lines and chords to client
-          if (err) {
-            returnObj.text = "FILE READ ERROR"
-            response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
-            response.end(JSON.stringify(returnObj))
-          } else {
-            var fileLines = data.toString().split("\n")
-            //get rid of any return characters
-            for (i in fileLines)
-              fileLines[i] = fileLines[i].replace(/(\r\n|\n|\r)/gm, "")
-            returnObj.text = songFile
-            returnObj.songLines = fileLines
-            returnObj.filePath = songFile
-            response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
-            response.end(JSON.stringify(returnObj))
-          }
-        })
+      if (validateJSON(urlObj.data, dataGetFormat)) {
+
+        let getUser = urlObj.data;
+        if (dataBase[getUser.user] === undefined) {
+          //username not found
+          returnObj.text = "USER NOT FOUND"
+          response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
+          response.end(JSON.stringify(returnObj))
+        }
+        else {
+
+          //add event to user entry in database
+          returnObj.data = dataBase[getUser.user]
+          response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
+          response.end(JSON.stringify(returnObj))
+
+        }
+
       }
-      else{
-           console.log(songFile + '<--DOES NOT EXIST')
-           response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
-           response.end(JSON.stringify(returnObj)) //send just the JSON object
-      }
-    })
+
+    }
+
+    else {
+      console.log(songFile + '<--DOES NOT EXIST')
+      response.writeHead(200, { "Content-Type": MIME_TYPES["json"] })
+      response.end(JSON.stringify(returnObj)) //send just the JSON object
+    }
+
 
   }
 
     if (request.method === "GET") {
-      //handle GET requests as static file requests
-      let filePath = ROOT_DIR + urlObj.pathname
-      if (urlObj.pathname === '/') filePath = ROOT_DIR + '/index.html'
+    //handle GET requests as static file requests
+    let filePath = ROOT_DIR + urlObj.pathname
+    if (urlObj.pathname === '/') filePath = ROOT_DIR + '/index.html'
 
-      fs.readFile(filePath, function(err, data) {
-        if (err) {
-          //report error to console
-          console.log('ERROR: ' + JSON.stringify(err))
-          //respond with not found 404 to client
-          response.writeHead(404)
-          response.end(JSON.stringify(err))
-          return
-        }
-        response.writeHead(200, {
-          'Content-Type': get_mime(filePath)
-        })
-        response.end(data)
+    fs.readFile(filePath, function (err, data) {
+      if (err) {
+        //report error to console
+        console.log('ERROR: ' + JSON.stringify(err))
+        //respond with not found 404 to client
+        response.writeHead(404)
+        response.end(JSON.stringify(err))
+        return
+      }
+      response.writeHead(200, {
+        'Content-Type': get_mime(filePath)
       })
-    }
-  })
+      response.end(data)
+    })
+  }
+})
 }).listen(3000)
 
 console.log('Server Running at http://127.0.0.1:3000  CNTL-C to quit')
